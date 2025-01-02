28898e0e99e1a228424e1b0159bc79e5ce9c6946
Sky <157863573+LuminescentSky@users.noreply.github.com>
2025-01-02 17:01:57 +0000

    didn't like having the changelog there

--- rom-patcher-js/more-extra-js.js
+++ rom-patcher-js/more-extra-js.js
@@ -4,19 +4,6 @@ document.getElementById("versionSelect").innerHTML += "<option value='2'>Speedwa
 
 document.getElementById("versionSelect").value = selectedVersion;
 
-document.getElementById("changelogDiv").innerHTML += "<h2>0.2 Beta</h2"
-document.getElementById("changelogDiv").innerHTML += "<li>Added custom Time Attack screens after beating Gnasty Gnorc and after 120% completion</li>"
-document.getElementById("changelogDiv").innerHTML += "<li>Added IGT to Inventory Screen</li>"
-document.getElementById("changelogDiv").innerHTML += "<li>Improved flight speed when initiating flight without momentum (this includes flight levels, making them more speedway viable)</li>"
-document.getElementById("changelogDiv").innerHTML += "<li>Added fixed heightcaps (set at same heights as SRT)</li>"
-document.getElementById("changelogDiv").innerHTML += "<li>Added manual sharp turn calibration (change offset by pressing R3 + Up/Down)</li>"
-document.getElementById("changelogDiv").innerHTML += "<li>Fixed an issue where the game could crash when supercharge jumping after rolling or roll bouncing.</li>"
-document.getElementById("changelogDiv").innerHTML += "<li>Removed the ability to change supercharge speed in-game (you can still set the supercharge speed from within the Mod Options)</li>"
-document.getElementById("changelogDiv").innerHTML += "<h2>0.1.1 Beta</h2"
-document.getElementById("changelogDiv").innerHTML += "<li>Fixed an issue where the game crashes when trying to view the 120% Ending</li>"
-document.getElementById("changelogDiv").innerHTML += "<h2>0.1 Beta</h2>"
-document.getElementById("changelogDiv").innerHTML += "<li>Initial Beta Release</li>"
-
 function changeVersion() {
 	if (document.getElementById("versionSelect").value == 0) {
 		window.location.href = "./0.1"
