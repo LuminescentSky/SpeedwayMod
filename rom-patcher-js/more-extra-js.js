d079a861652731d34422504339efa81320240bab
Sky <157863573+LuminescentSky@users.noreply.github.com>
2025-01-08 14:33:39 +0000

    couldn't select 0.3.1

--- rom-patcher-js/more-extra-js.js
+++ rom-patcher-js/more-extra-js.js
@@ -2,7 +2,7 @@ document.getElementById("versionSelect").innerHTML += "<option value='0'>Speedwa
 document.getElementById("versionSelect").innerHTML += "<option value='1'>Speedway Mod 0.1.1 Beta</option>";
 document.getElementById("versionSelect").innerHTML += "<option value='2'>Speedway Mod 0.2 Beta</option>";
 document.getElementById("versionSelect").innerHTML += "<option value='3'>Speedway Mod 0.3 Beta (PAL only)</option>";
-document.getElementById("versionSelect").innerHTML += "<option value='3'>Speedway Mod 0.3.1 Beta (Latest, PAL only)</option>";
+document.getElementById("versionSelect").innerHTML += "<option value='4'>Speedway Mod 0.3.1 Beta (Latest, PAL only)</option>";
 
 document.getElementById("versionSelect").value = selectedVersion;
 
