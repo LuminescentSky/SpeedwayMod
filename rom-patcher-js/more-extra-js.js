3d1d0e1d9884f7cb2eaed191d1ee9c390e5b0030
Sky <157863573+LuminescentSky@users.noreply.github.com>
2025-01-13 23:13:45 +0000

    0.3.2 Beta

--- rom-patcher-js/more-extra-js.js
+++ rom-patcher-js/more-extra-js.js
@@ -2,7 +2,8 @@ document.getElementById("versionSelect").innerHTML += "<option value='0'>Speedwa
 document.getElementById("versionSelect").innerHTML += "<option value='1'>Speedway Mod 0.1.1 Beta</option>";
 document.getElementById("versionSelect").innerHTML += "<option value='2'>Speedway Mod 0.2 Beta</option>";
 document.getElementById("versionSelect").innerHTML += "<option value='3'>Speedway Mod 0.3 Beta (PAL only)</option>";
-document.getElementById("versionSelect").innerHTML += "<option value='4'>Speedway Mod 0.3.1 Beta (Latest, PAL only)</option>";
+document.getElementById("versionSelect").innerHTML += "<option value='4'>Speedway Mod 0.3.1 Beta (PAL only)</option>";
+document.getElementById("versionSelect").innerHTML += "<option value='5'>Speedway Mod 0.3.2 Beta (Latest, PAL only)</option>";
 
 document.getElementById("versionSelect").value = selectedVersion;
 
@@ -17,6 +18,8 @@ function changeVersion() {
 		window.location.href = "./0.3"
 	} else if (document.getElementById("versionSelect").value == 4) {
 		window.location.href = "./0.3.1"
+	} else if (document.getElementById("versionSelect").value == 5) {
+		window.location.href = "./0.3.2"
 	}
 }
 
